ee1a508018617bb44e0e77de8e789469f0184321
Jaxon Smith <96396741+STGNinja@users.noreply.github.com>
2023-08-15 22:21:41 -0600

    Add files via upload

---
+++ tabs-orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Approved',
+        productNumber: '5/5',
+        paymentStatus: '40',
+        status: 'I Love To See The Temple'
+    },
+    {
+        productName: 'Approved',
+        productNumber: '4/5',
+        paymentStatus: '37',
+        status: 'Gesthemane'
+    },
+    {
+        productName: 'Approved',
+        productNumber: '4/5',
+        paymentStatus: '24',
+        status: 'Come, Come, Ye, Saints'
+    },
+]
