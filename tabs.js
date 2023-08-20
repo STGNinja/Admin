8956700459321af974c7291552028b892f26aa44
Jaxon Smith <96396741+STGNinja@users.noreply.github.com>
2023-08-19 18:31:38 -0600

    Add files via upload

--- tabs.js
+++ tabs.js
@@ -71,6 +71,19 @@ updateDarkMode(); // Apply dark mode on page load
 
 
 
+// Function to close the notification
+function closeNotification() {
+    const notification = document.getElementById('notification');
+    notification.style.animation = 'slideUp 1s ease-in-out forwards';
+    
+    // Remove the notification from the DOM after animation
+    setTimeout(() => {
+        notification.style.display = 'none';
+    }, 1000); // Wait for 1 second (animation duration)
+}
+
+
+
 
 
 
